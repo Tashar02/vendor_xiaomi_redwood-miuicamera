0580cb274ddc07661bcb6616c5e222c0e56e7952
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-03-19 03:28:29 +0600

    redwood-miuicamera: Specify bool for override_format_from_reserved soong config
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/xiaomi/redwood-miuicamera/BoardConfig.mk)
 $(call inherit-product, vendor/xiaomi/redwood-miuicamera/vendor/vendor-vendor.mk)
 
 # Camera
-$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Public libraries
 PRODUCT_COPY_FILES += \
